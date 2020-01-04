9a940fad8b3d93addfae3b4a18e1ee71f59b7003
marumimumemo <sagaminumber3@yahoo.co.jp>
2020-01-04 22:22:22 +0900

    Initial Commit

---
+++ userDidTakeScreenshotNotificationSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  userDidTakeScreenshotNotificationSample
+//
+//  Created by 丸本聡 on 2020/01/04.
+//  Copyright © 2020 丸本聡. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
